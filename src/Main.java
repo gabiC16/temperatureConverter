39fc60ce67e48c1ea533bd0c8cf57f8d7da3c8e6
“gabic50” <gcoope201@caledonian.ac.uk>
2025-05-26 21:32:42 +0100

    Upload temperature converter program

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,24 @@ public class Main {
 
         //Temperature Converter
 
+        //Declare variables
 
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.println("Enter the temperature: ");
+        temp = scanner.nextDouble();
+
+        System.out.println("Convert to Celsius or Fahrenheit? (C or F): ");
+        unit = scanner.next().toUpperCase();
+
+        // ternary operator
+        // (condition) ? true : false
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 9 / 5) + 32;
+
+        System.out.printf("%.1f%s",newTemp, unit);
+
+        scanner.close();
     }
 }
